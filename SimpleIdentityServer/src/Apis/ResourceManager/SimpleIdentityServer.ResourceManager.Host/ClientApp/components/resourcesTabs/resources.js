212d5f0e99483fe2d6a1fe32557a1519f0c2c30c
thabart <habarthierry@hotmail.fr>
2018-05-18 13:13:33 +0200

    Create & delete resource

--- SimpleIdentityServer/src/Apis/ResourceManager/SimpleIdentityServer.ResourceManager.Host/ClientApp/components/resourcesTabs/resources.js
+++ SimpleIdentityServer/src/Apis/ResourceManager/SimpleIdentityServer.ResourceManager.Host/ClientApp/components/resourcesTabs/resources.js
@@ -2,12 +2,18 @@ import React, { Component } from "react";
 import { translate } from 'react-i18next';
 import { ResourceService } from '../../services';
 import { withRouter } from 'react-router-dom';
+import { ChipsSelector } from '../common';
 
 import Table, { TableBody, TableCell, TableHead, TableRow, TableFooter, TablePagination } from 'material-ui/Table';
-import { Popover, IconButton, Menu, MenuItem, Checkbox, TextField, Select, Avatar, CircularProgress } from 'material-ui';
+import { Popover, IconButton, Menu, MenuItem, Checkbox, TextField, Select, Avatar, CircularProgress, Button } from 'material-ui';
+import Dialog, { DialogTitle, DialogContent, DialogActions } from 'material-ui/Dialog';
+import Input, { InputLabel } from 'material-ui/Input';
+import { FormControl, FormHelperText } from 'material-ui/Form';
 import Delete from '@material-ui/icons/Delete';
 import MoreVert from '@material-ui/icons/MoreVert';
 import Visibility from '@material-ui/icons/Visibility';
+import AppDispatcher from '../../appDispatcher';
+import Constants from '../../constants';
 
 class Resources extends Component {
     constructor(props) {
@@ -18,14 +24,35 @@ class Resources extends Component {
         this.handleRowClick = this.handleRowClick.bind(this);
         this.refreshData = this.refreshData.bind(this);
         this.handleRemoveResources = this.handleRemoveResources.bind(this);
+        this.handleCloseModal = this.handleCloseModal.bind(this);
+        this.handleOpenModal = this.handleOpenModal.bind(this);
+        this.handleAddResource = this.handleAddResource.bind(this);
+        this.handleChangeResource = this.handleChangeResource.bind(this);
         this.state = {
             isLoading: false,
             page: 0,
             pageSize: 5,
             isRemoveDisplayed: false,
             anchorEl: null,
+            isModalOpened: false,
+            isAddResourceLoading: false,
+            newResource: {
+                scopes: []
+            }
         };
     }
+
+    /**
+    * Change the resource information.
+    */
+    handleChangeResource(e) {
+        var newResource = this.state.newResource;
+        newResource[e.target.name] = e.target.value;
+        this.setState({
+            newResource: newResource
+        });
+    }
+
     /**
     * Open the menu.
     */
@@ -41,6 +68,36 @@ class Resources extends Component {
     handleClose() {
         this.setState({
             anchorEl: null
+        });
+    }
+
+    /**
+    * Add the resource.
+    */
+    handleAddResource() {
+        var self = this;
+        const {t} = self.props;
+        self.setState({
+            isAddResourceLoading: true
+        });
+        ResourceService.add(self.state.newResource).then(function() {
+            self.setState({
+                isAddResourceLoading: false,
+                isModalOpened: false
+            });
+            AppDispatcher.dispatch({
+                actionName: Constants.events.DISPLAY_MESSAGE,
+                data: t('resourceAdded')
+            });
+            self.refreshData();
+        }).catch(function() {
+            AppDispatcher.dispatch({
+                actionName: Constants.events.DISPLAY_MESSAGE,
+                data: t('resourceCannotBeAdded')
+            });
+            self.setState({
+                isAddResourceLoading: false
+            });
         });
     }
 
@@ -115,7 +172,59 @@ class Resources extends Component {
     * Remove the selected resources.
     */
     handleRemoveResources() {
+        var self = this;
+        var resourceIds = self.state.data.filter(function(d) { return d.isSelected; }).map(function(d) { return d.id; });
+        if(resourceIds.length === 0) {
+            return;
+        }
 
+        const { t } = self.props;
+        self.setState({
+            isLoading: true
+        });
+        var operations = [];
+        resourceIds.forEach(function(resourceId) {
+            operations.push(ResourceService.delete(resourceId));
+        });
+        Promise.all(operations).then(function() {
+            AppDispatcher.dispatch({
+                actionName: Constants.events.DISPLAY_MESSAGE,
+                data: t('resourcesAreRemoved')
+            });
+            self.setState({
+                isRemoveDisplayed: false
+            });
+            self.refreshData();
+        }).catch(function(e) {
+            self.setState({
+                isLoading: false
+            });
+            AppDispatcher.dispatch({
+                actionName: Constants.events.DISPLAY_MESSAGE,
+                data: t('resourcesCannotBeRemoved')
+            });
+        });
+    }
+
+    /**
+    * Close the modal.
+    */
+    handleCloseModal() {
+        this.setState({
+            isModalOpened: false
+        });
+    }
+
+    /**
+    * Open the modal.
+    */
+    handleOpenModal() {
+        this.setState({
+            isModalOpened: true,
+            newResource: {
+                scopes: []
+            }
+        });
     }
 
     render() {
@@ -139,53 +248,83 @@ class Resources extends Component {
             });
         }
 
-        return (<div className="card">
-                <div className="header">
-                    <h4 style={{display: "inline-block"}}>{t('resources')}</h4>
-                    <div style={{float: "right"}}>
-                        {self.state.isRemoveDisplayed && (
-                            <IconButton onClick={self.handleRemoveResources}>
-                                <Delete />
-                            </IconButton>
-                        )}
-                        <IconButton onClick={this.handleClick}>
-                            <MoreVert />
-                        </IconButton>
-                        <Menu anchorEl={self.state.anchorEl} open={Boolean(self.state.anchorEl)} onClose={self.handleClose}>
-                            <MenuItem>{t('addResource')}</MenuItem>
-                        </Menu>
-                    </div>
-                </div>
-                <div className="body">
-                    { this.state.isLoading ? (<CircularProgress />) : (
+        return (<div>
+                <Dialog open={self.state.isModalOpened} onClose={this.handleCloseModal}>
+                    <DialogTitle>{t('addResource')}</DialogTitle>
+                    {self.state.isAddResourceLoading ? (<CircularProgress />) : (
                         <div>
-                            <Table>
-                                <TableHead>
-                                    <TableRow>
-                                        <TableCell></TableCell>
-                                        <TableCell>{t('resourceId')}</TableCell>
-                                        <TableCell>{t('resourceName')}</TableCell>
-                                        <TableCell>{t('resourceType')}</TableCell>
-                                        <TableCell>{t('resourceScopes')}</TableCell>
-                                        <TableCell></TableCell>
-                                    </TableRow>
-                                </TableHead>
-                                <TableBody>
-                                    <TableRow>
-                                        <TableCell><Checkbox color="primary" onChange={self.handleAllSelections} /></TableCell>
-                                        <TableCell></TableCell>
-                                        <TableCell></TableCell>
-                                        <TableCell></TableCell>
-                                        <TableCell></TableCell>
-                                        <TableCell></TableCell>
-                                    </TableRow>
-                                    {rows}
-                                </TableBody>
-                            </Table>
-                            <TablePagination component="div" count={self.state.count} rowsPerPage={self.state.pageSize} page={this.state.page} onChangePage={self.handleChangePage} onChangeRowsPerPage={self.handleChangeRowsPage} />
+                            <DialogContent>
+                                {/* Resource name */ }
+                                <FormControl fullWidth={true}>
+                                    <InputLabel htmlFor="provider">{t('resourceName')}</InputLabel>
+                                    <Input id="provider" value={self.state.newResource.name} name="name" onChange={(e) => { self.handleChangeResource(e); }}  />
+                                    <FormHelperText>{t('resourceNameDescription')}</FormHelperText>
+                                </FormControl>
+                                {/* Resource type */ }
+                                <FormControl fullWidth={true}>
+                                    <InputLabel htmlFor="provider">{t('resourceType')}</InputLabel>
+                                    <Input id="provider" value={self.state.newResource.type} name="type" onChange={(e) => { self.handleChangeResource(e); }}  />
+                                    <FormHelperText>{t('resourceTypeDescription')}</FormHelperText>
+                                </FormControl>
+                                {/* Resource scopes */}
+                                <FormControl fullWidth={true}>
+                                    <ChipsSelector label={t('resourceScopes')} properties={self.state.newResource.scopes} />
+                                </FormControl>
+                            </DialogContent>
+                            <DialogActions>
+                                <Button  variant="raised" color="primary" onClick={self.handleAddResource}>{t('addResource')}</Button>
+                            </DialogActions>
                         </div>
                     )}
-                </div>
+                </Dialog>
+                <div className="card">
+                    <div className="header">
+                        <h4 style={{display: "inline-block"}}>{t('resources')}</h4>
+                        <div style={{float: "right"}}>
+                            {self.state.isRemoveDisplayed && (
+                                <IconButton onClick={self.handleRemoveResources}>
+                                    <Delete />
+                                </IconButton>
+                            )}
+                            <IconButton onClick={this.handleClick}>
+                                <MoreVert />
+                            </IconButton>
+                            <Menu anchorEl={self.state.anchorEl} open={Boolean(self.state.anchorEl)} onClose={self.handleClose}>
+                                <MenuItem onClick={self.handleOpenModal}>{t('addResource')}</MenuItem>
+                            </Menu>
+                        </div>
+                    </div>
+                    <div className="body">
+                        { this.state.isLoading ? (<CircularProgress />) : (
+                            <div>
+                                <Table>
+                                    <TableHead>
+                                        <TableRow>
+                                            <TableCell></TableCell>
+                                            <TableCell>{t('resourceId')}</TableCell>
+                                            <TableCell>{t('resourceName')}</TableCell>
+                                            <TableCell>{t('resourceType')}</TableCell>
+                                            <TableCell>{t('resourceScopes')}</TableCell>
+                                            <TableCell></TableCell>
+                                        </TableRow>
+                                    </TableHead>
+                                    <TableBody>
+                                        <TableRow>
+                                            <TableCell><Checkbox color="primary" onChange={self.handleAllSelections} /></TableCell>
+                                            <TableCell></TableCell>
+                                            <TableCell></TableCell>
+                                            <TableCell></TableCell>
+                                            <TableCell></TableCell>
+                                            <TableCell></TableCell>
+                                        </TableRow>
+                                        {rows}
+                                    </TableBody>
+                                </Table>
+                                <TablePagination component="div" count={self.state.count} rowsPerPage={self.state.pageSize} page={this.state.page} onChangePage={self.handleChangePage} onChangeRowsPerPage={self.handleChangeRowsPage} />
+                            </div>
+                        )}
+                    </div>
+            </div>
         </div>);
     }
 
