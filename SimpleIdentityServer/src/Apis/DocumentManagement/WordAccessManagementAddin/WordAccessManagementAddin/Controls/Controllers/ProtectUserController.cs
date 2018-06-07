7fa28c2cc7ca6d6882fd2bbbda793e28266743dc
thabart <habarthierry@hotmail.fr>
2018-06-07 15:42:30 +0200

    Display a fake UI to assign the permissions

--- SimpleIdentityServer/src/Apis/DocumentManagement/WordAccessManagementAddin/WordAccessManagementAddin/Controls/Controllers/ProtectUserController.cs
+++ SimpleIdentityServer/src/Apis/DocumentManagement/WordAccessManagementAddin/WordAccessManagementAddin/Controls/Controllers/ProtectUserController.cs
@@ -1,6 +1,80 @@
-﻿namespace WordAccessManagementAddin.Controls.Controllers
+﻿using System.Linq;
+using WordAccessManagementAddin.Controls.ViewModels;
+
+namespace WordAccessManagementAddin.Controls.Controllers
 {
     internal sealed class ProtectUserController
     {
+        public ProtectUserController()
+        {
+            ViewModel = new ProtectUserViewModel();
+            ViewModel.Permissions.Add(new PermissionViewModel
+            {
+                IsSelected = false,
+                Name = "read"
+            });
+            ViewModel.PermissionAdded += HandleAddPermission;
+            ViewModel.PermissionsRemoved += HandlePermissionsRemoved;
+            ViewModel.PermissionsSaved += HandlePermissionsSaved;
+        }
+
+        /// <summary>
+        /// Save the permissions.
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void HandlePermissionsSaved(object sender, System.EventArgs e)
+        {
+
+        }
+
+        /// <summary>
+        /// Remove the selected users.
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void HandlePermissionsRemoved(object sender, System.EventArgs e)
+        {
+            var selectedUsers = ViewModel.Users.Where(p => p.IsSelected);
+            if (!selectedUsers.Any())
+            {
+                return;
+            }
+
+            var names = selectedUsers.Select(u => u.Name).ToList();
+            foreach(var name in names)
+            {
+                ViewModel.Users.Remove(ViewModel.Users.First(u => u.Name == name));
+            }
+        }
+
+        /// <summary>
+        /// Add a permission.
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void HandleAddPermission(object sender, System.EventArgs e)
+        {
+            var userIdentifier = ViewModel.UserIdentifier;
+            var selectedPermissions = ViewModel.Permissions.Where(p => p.IsSelected);
+            if (string.IsNullOrWhiteSpace(userIdentifier) || !selectedPermissions.Any() || ViewModel.Users.Any(u => u.Name == userIdentifier))
+            {
+                return;
+            }
+
+            ViewModel.UserIdentifier = string.Empty;
+            ViewModel.Users.Add(new UserViewModel
+            {
+                IsSelected = false,
+                Name = userIdentifier,
+                Permissions = selectedPermissions.Select(s => s.Name)
+            });
+            foreach(var permission in ViewModel.Permissions)
+            {
+                permission.IsSelected = false;
+            }
+        }
+
+        public ProtectUserViewModel ViewModel { get; set; }
     }
 }
--- SimpleIdentityServer/src/Apis/DocumentManagement/WordAccessManagementAddin/WordAccessManagementAddin/Controls/ViewModels/ProtectUserViewModel.cs
+++ SimpleIdentityServer/src/Apis/DocumentManagement/WordAccessManagementAddin/WordAccessManagementAddin/Controls/ViewModels/ProtectUserViewModel.cs
@@ -1,7 +1,172 @@
-﻿namespace WordAccessManagementAddin.Controls.ViewModels
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Windows.Input;
+
+namespace WordAccessManagementAddin.Controls.ViewModels
 {
+    internal sealed class PermissionViewModel : BaseViewModel
+    {
+        private string _name;
+        private bool _isSelected;
+
+        public string Name
+        {
+            get
+            {
+                return _name;
+            }
+            set
+            {
+                if (_name != value)
+                {
+                    _name = value;
+                }
+            }
+        }
+
+        public bool IsSelected
+        {
+            get
+            {
+                return _isSelected;
+            }
+            set
+            {
+                if (_isSelected != value)
+                {
+                    _isSelected = true;
+                }
+            }
+        }
+    }
+
+    internal sealed class UserViewModel : BaseViewModel
+    {
+        private string _name;
+        private IEnumerable<string> _permissions;
+        private bool _isSelected;
+
+        public string Name
+        {
+            get
+            {
+                return _name;
+            }
+            set
+            {
+                if (_name != value)
+                {
+                    _name = value;
+                }
+            }
+        }
+
+        public bool IsSelected
+        {
+            get
+            {
+                return _isSelected;
+            }
+            set
+            {
+                if (_isSelected != value)
+                {
+                    _isSelected = true;
+                }
+            }
+        }
+
+        public IEnumerable<string> Permissions
+        {
+            get
+            {
+                return _permissions;
+            }
+            set
+            {
+                if(_permissions != value)
+                {
+                    _permissions = value;
+                }
+            }
+        }
+    }
+
     internal sealed class ProtectUserViewModel : BaseViewModel
     {
+        private string _userIdentifier;
 
+        public ProtectUserViewModel()
+        {
+            Permissions = new ObservableCollection<PermissionViewModel>();
+            Users = new ObservableCollection<UserViewModel>();
+            AddCommand = new RelayCommand(HandleAddPermission, p => CanExecuteAddPermission());
+            RemoveCommand = new RelayCommand(HandleRemovePermissions, p => CanExecuteRemovePermissions());
+            SaveCommand = new RelayCommand(HandleSavePermissions, p => CanSavePermissions());
+        }
+
+        public string UserIdentifier
+        {
+            get
+            {
+                return _userIdentifier;
+            }
+            set
+            {
+                if (_userIdentifier != value)
+                {
+                    _userIdentifier = value;
+                    OnPropertyChanged("UserIdentifier");
+                }
+            }
+        }
+        public ObservableCollection<PermissionViewModel> Permissions { get; set; }
+        public ObservableCollection<UserViewModel> Users { get; set; }
+        public ICommand AddCommand { get; private set; }
+        public ICommand RemoveCommand { get; private set; }
+        public ICommand SaveCommand { get; private set; }
+        public event EventHandler PermissionAdded;
+        public event EventHandler PermissionsRemoved;
+        public event EventHandler PermissionsSaved;
+
+        private void HandleAddPermission(object o)
+        {
+            if (PermissionAdded != null)
+            {
+                PermissionAdded(this, EventArgs.Empty);
+            }
+        }
+
+        private bool CanExecuteAddPermission()
+        {
+            return true;
+        }
+
+        private void HandleRemovePermissions(object o)
+        {
+            if (PermissionsRemoved != null)
+            {
+                PermissionsRemoved(this, EventArgs.Empty);
+            }
+        }
+
+        private bool CanExecuteRemovePermissions()
+        {
+            return true;
+        }
+
+        private void HandleSavePermissions(object o)
+        {
+            if (PermissionsSaved != null)
+            {
+                PermissionsSaved(this, EventArgs.Empty);
+            }
+        }
+
+        private bool CanSavePermissions()
+        {
+            return true;
+        }
     }
 }
